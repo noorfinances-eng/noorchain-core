35f11a8e90e78b1b2ab7a5d0b5b297827c009d03
noorfinances-eng <noorfinances@gmail.com>
2025-11-17 16:09:08 +0100

    Add Name and Version to App struct
    Initialize App struct with Name and Version fields and update NewNoorchainApp function to return a populated instance.

--- app/app.go
+++ app/app.go
@@ -1,13 +1,34 @@
 package app
 
-// App is a placeholder for the NOORCHAIN Cosmos/Ethermint application.
-// Later we will integrate Cosmos SDK modules and Ethermint here.
+import "fmt"
+
+// App is a placeholder for the NOORCHAIN application.
+//
+// In the next steps, this struct will be extended to embed the Cosmos SDK
+// base application and the Ethermint (EVM) modules.
 type App struct {
-	// TODO: add Cosmos SDK baseapp and keepers
+    Name    string
+    Version string
 }
 
-// NewNoorchainApp will later initialize the full NOORCHAIN app.
-// For now it just returns an empty placeholder App.
+// NewNoorchainApp creates a new placeholder instance of the NOORCHAIN app.
+//
+// Later, this function will take many parameters (logger, database, encoding,
+// app options, etc.) but for now we keep it minimal so that the project
+// structure is clear and buildable.
 func NewNoorchainApp() *App {
-	return &App{}
+    return &App{
+        Name:    "NOORCHAIN",
+        Version: "0.0.1-dev",
+    }
+}
+
+// Start is a placeholder method that will later start the full node logic.
+//
+// For now, it just prints a message. In future steps, this will be replaced
+// by proper Cosmos SDK + Ethermint wiring and ABCI server startup.
+func (a *App) Start() error {
+    fmt.Printf("%s node (version %s) starting...\n", a.Name, a.Version)
+    fmt.Println("Cosmos SDK + Ethermint wiring will be added in the next technical phases.")
+    return nil
 }
