3862508d8a9c7646bca0314619d1cb66a5003877
noorfinances-eng <noorfinances@gmail.com>
2025-11-21 20:38:16 +0100

    Update app_builder.go

--- app/app_builder.go
+++ app/app_builder.go
@@ -4,20 +4,27 @@ import (
 	"io"
 
 	dbm "github.com/cosmos/cosmos-db"
+
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	// --- Cosmos SDK keepers ---
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
 
+	// --- Ethermint (EVM) keepers ---
 	evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
 	feemarketkeeper "github.com/evmos/ethermint/x/feemarket/keeper"
 
+	// --- NOORCHAIN PoSS ---
 	noorsignalkeeper "github.com/noorfinances-eng/noorchain-core/x/noorsignal/keeper"
 )
 
-// AppBuilder construit progressivement l'application Cosmos SDK complète.
+// ------------------------------------------------------------
+// AppBuilder : construit progressivement NOORCHAIN
+// ------------------------------------------------------------
 type AppBuilder struct {
 	logger     sdk.Logger
 	db         dbm.DB
@@ -29,7 +36,9 @@ type AppBuilder struct {
 	storeKeys StoreKeys
 }
 
-// NewAppBuilder crée un nouveau builder.
+// ------------------------------------------------------------
+// NewAppBuilder : constructeur
+// ------------------------------------------------------------
 func NewAppBuilder(
 	logger sdk.Logger,
 	db dbm.DB,
@@ -51,15 +60,28 @@ func NewAppBuilder(
 	}
 }
 
-func (b *AppBuilder) EncodingConfig() EncodingConfig { return b.encCfg }
-func (b *AppBuilder) StoreKeys() StoreKeys           { return b.storeKeys }
+// ------------------------------------------------------------
+// EncodingConfig getter
+// ------------------------------------------------------------
+func (b *AppBuilder) EncodingConfig() EncodingConfig {
+	return b.encCfg
+}
 
-// BuildBaseApp crée une instance BaseApp complète (stores montés).
+// ------------------------------------------------------------
+// StoreKeys getter
+// ------------------------------------------------------------
+func (b *AppBuilder) StoreKeys() StoreKeys {
+	return b.storeKeys
+}
+
+// ------------------------------------------------------------
+// BuildBaseApp : crée BaseApp + monte les stores KV
+// ------------------------------------------------------------
 func (b *AppBuilder) BuildBaseApp() *baseapp.BaseApp {
 	sk := b.storeKeys
 	txDecoder := b.encCfg.TxConfig.TxDecoder()
 
-	// 1) BaseApp minimale
+	// 1) BaseApp
 	base := baseapp.NewBaseApp(
 		AppName,
 		b.logger,
@@ -68,27 +90,27 @@ func (b *AppBuilder) BuildBaseApp() *baseapp.BaseApp {
 		baseapp.SetChainID(ChainID),
 	)
 
-	// 2) Monter les KVStores
+	// 2) Mount KVStores
 	if base != nil {
-		// --- Stores Cosmos classiques ---
+		// --- Stores Cosmos classiques
 		base.MountKVStore(sk.AuthKey)
 		base.MountKVStore(sk.BankKey)
 		base.MountKVStore(sk.StakingKey)
 		base.MountKVStore(sk.GovKey)
 		base.MountKVStore(sk.ParamsKey)
 
-		// --- Store PoSS ---
+		// --- Store PoSS
 		base.MountKVStore(sk.NoorSignalKey)
 
-		// --- EVM Stores (Ethermint) ---
+		// --- Stores Ethermint (EVM)
 		base.MountKVStore(sk.EvmKey)
 		base.MountKVStore(sk.FeeMarketKey)
 
-		// --- Transient store ---
+		// --- Transient store
 		base.MountTransientStore(sk.ParamsTransientKey)
 	}
 
-	// 3) Charger la version si demandé
+	// 3) Load version
 	if b.loadLatest && base != nil {
 		if err := base.LoadLatestVersion(); err != nil {
 			panic(err)
@@ -98,11 +120,15 @@ func (b *AppBuilder) BuildBaseApp() *baseapp.BaseApp {
 	return base
 }
 
-// BuildKeepers crée les keepers (version minimaliste pour l’instant).
+// ------------------------------------------------------------
+// BuildKeepers : crée les keepers principaux
+// (les keepers EVM seront branchés plus tard)
+// ------------------------------------------------------------
 func (b *AppBuilder) BuildKeepers() AppKeepers {
 	sk := b.storeKeys
 	enc := b.encCfg
 
+	// 1) ParamsKeeper
 	paramsKeeper := paramskeeper.NewKeeper(
 		enc.Marshaler,
 		enc.Amino,
@@ -110,6 +136,7 @@ func (b *AppBuilder) BuildKeepers() AppKeepers {
 		sk.ParamsTransientKey,
 	)
 
+	// 2) AccountKeeper
 	maccPerms := map[string][]string{}
 	accountKeeper := authkeeper.NewAccountKeeper(
 		enc.Marshaler,
@@ -119,6 +146,7 @@ func (b *AppBuilder) BuildKeepers() AppKeepers {
 		authtypes.ProtoBaseAccount,
 	)
 
+	// 3) BankKeeper
 	blockedAddrs := map[string]bool{}
 	bankKeeper := bankkeeper.NewBaseKeeper(
 		enc.Marshaler,
@@ -128,21 +156,23 @@ func (b *AppBuilder) BuildKeepers() AppKeepers {
 		"",
 	)
 
+	// 4) NoorSignalKeeper (PoSS)
 	noorSignalKeeper := noorsignalkeeper.NewKeeper(
 		enc.Marshaler,
 		sk.NoorSignalKey,
 	)
 
-	// Keepers EVM non encore initialisés
-	var evmKeeper evmkeeper.Keeper
-	var feeMarketKeeper feemarketkeeper.Keeper
+	// 5) EVM keepers → initialisés plus tard
+	var evmKeeperPtr *evmkeeper.Keeper = nil
+	var feeMarketKeeperPtr *feemarketkeeper.Keeper = nil
 
+	// 6) Retourner les keepers
 	return AppKeepers{
 		AccountKeeper:    accountKeeper,
 		BankKeeper:       bankKeeper,
 		ParamsKeeper:     paramsKeeper,
-		EvmKeeper:        evmKeeper,
-		FeeMarketKeeper:  feeMarketKeeper,
 		NoorSignalKeeper: noorSignalKeeper,
+		EvmKeeper:        evmKeeperPtr,
+		FeeMarketKeeper:  feeMarketKeeperPtr,
 	}
 }
