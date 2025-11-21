5404c7666b378d7071502800d522b88ffa08c7b8
noorfinances-eng <noorfinances@gmail.com>
2025-11-21 21:57:08 +0100

    Update genesis.go

--- app/genesis.go
+++ app/genesis.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"encoding/json"
+
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
@@ -12,15 +15,15 @@ import (
 //
 // Les adresses sont centralisées dans x/noorsignal/types/addresses.go
 // et peuvent être remplacées plus tard sans toucher au code.
-//
-// Cette fonction est appelée dans InitChainer,
-// avant le ModuleManager.InitGenesis().
-func ApplyEconomicGenesis(genesisState map[string]json.RawMessage, cdc codec.JSONCodec) {
+func ApplyEconomicGenesis(
+	genesisState map[string]json.RawMessage,
+	cdc codec.JSONCodec,
+) {
 
 	// 1) Récupérer la partie bank du genesis
 	var bankState banktypes.GenesisState
-	if genesisState[banktypes.ModuleName] != nil {
-		if err := cdc.UnmarshalJSON(genesisState[banktypes.ModuleName], &bankState); err != nil {
+	if bz, ok := genesisState[banktypes.ModuleName]; ok && bz != nil {
+		if err := cdc.UnmarshalJSON(bz, &bankState); err != nil {
 			panic(err)
 		}
 	} else {
@@ -29,49 +32,42 @@ func ApplyEconomicGenesis(genesisState map[string]json.RawMessage, cdc codec.JSO
 
 	// 2) Récupérer les adresses officielles (réelles ou placeholders)
 	foundationAddr := noorsignaltypes.GetFoundationAddress()
-	devAddr        := noorsignaltypes.GetDevWalletAddress()
-	stimulusAddr   := noorsignaltypes.GetStimulusAddress()
-	presaleAddr    := noorsignaltypes.GetPresaleAddress()
+	devAddr := noorsignaltypes.GetDevWalletAddress()
+	stimulusAddr := noorsignaltypes.GetStimulusAddress()
+	presaleAddr := noorsignaltypes.GetPresaleAddress()
 	possReserveAddr := noorsignaltypes.GetPossReserveAddress()
 
-	// 3) Définir les montants des comptes (en unur)
-	//    Basé sur la répartition 5 / 5 / 5 / 5 / 80 %
-	//    1 NUR = 1'000'000 unur
-
+	// 3) Définir les montants (en unur)
 	const (
-		totalSupply       uint64 = 299_792_458
-		allocation5Pct    uint64 = totalSupply * 5 / 100     // 14 989 622
-		allocation80Pct   uint64 = totalSupply * 80 / 100    // 239 833 966
-		micro             uint64 = 1_000_000                 // unur per NUR
+		totalSupply     uint64 = 299_792_458
+		allocation5Pct  uint64 = totalSupply * 5 / 100   // 14 989 622
+		allocation80Pct uint64 = totalSupply * 80 / 100  // 239 833 966
+		micro           uint64 = 1_000_000               // 1 NUR = 1e6 unur
 	)
 
-	amount5pct  := sdk.NewInt(int64(allocation5Pct * micro))
-	amount80pct := sdk.NewInt(int64(allocation80Pct * micro))
+	amount5 := sdk.NewIntFromUint64(allocation5Pct * micro)
+	amount80 := sdk.NewIntFromUint64(allocation80Pct * micro)
 
-	// 4) Ajouter les balances au genesis
-	addBalance := func(addr string, amount sdk.Int) {
+	// 4) Helper pour ajouter une balance + mettre à jour la supply
+	addBalance := func(addr string, amt sdk.Int) {
+		if addr == "" {
+			// Si jamais une adresse réelle n'est pas encore renseignée,
+			// on ne crée pas d'entrée vide dans le genesis.
+			return
+		}
 		bankState.Balances = append(bankState.Balances, banktypes.Balance{
 			Address: addr,
-			Coins:   sdk.NewCoins(sdk.NewCoin("unur", amount)),
+			Coins:   sdk.NewCoins(sdk.NewCoin("unur", amt)),
 		})
+		bankState.Supply = bankState.Supply.Add(sdk.NewCoin("unur", amt))
 	}
 
-	addBalance(foundationAddr, amount5pct)
-	addBalance(devAddr, amount5pct)
-	addBalance(stimulusAddr, amount5pct)
-	addBalance(presaleAddr, amount5pct)
-	addBalance(possReserveAddr, amount80pct)
-
-	// 5) Mettre à jour supply totale
-	totalCoins := bankState.Supply.Add(
-		sdk.NewCoin("unur", amount5pct),
-		sdk.NewCoin("unur", amount5pct),
-		sdk.NewCoin("unur", amount5pct),
-		sdk.NewCoin("unur", amount5pct),
-		sdk.NewCoin("unur", amount80pct),
-	)
-
-	bankState.Supply = totalCoins
+	// 5) Appliquer les 5 poches économiques
+	addBalance(foundationAddr, amount5)
+	addBalance(devAddr, amount5)
+	addBalance(stimulusAddr, amount5)
+	addBalance(presaleAddr, amount5)
+	addBalance(possReserveAddr, amount80)
 
 	// 6) Ré-encoder la section bank du genesis
 	bz, err := cdc.MarshalJSON(&bankState)
