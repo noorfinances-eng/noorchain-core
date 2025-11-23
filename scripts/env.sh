7a8856c83fd8e94ef18616a870843d56bf767551
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:30:06 +0100

    Create env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# NOORCHAIN Phase 2 environment placeholder.
+# Variables will be expanded in later steps for the Testnet.
+
+export NOORCHAIN_ENV="phase2"
+export NOORCHAIN_CHAIN_ID="noorchain_9000-1"
