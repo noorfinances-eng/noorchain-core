0fdb94a32b2ff4d7c96fd2145bc4b0812d24a861
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:10:16 +0100

    Create options.go

---
+++ cmd/noord/options.go
@@ -0,0 +1,10 @@
+package main
+
+// Options represents placeholder CLI options for NOORCHAIN.
+// It will be expanded in later phases when flags and configs are added.
+type Options struct{}
+
+// DefaultOptions returns an empty Options placeholder.
+func DefaultOptions() Options {
+	return Options{}
+}
