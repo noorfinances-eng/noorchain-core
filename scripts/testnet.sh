b83003e7a821e4987c9d72eaac3a0109e313cd08
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:30:49 +0100

    Create testnet.sh

---
+++ scripts/testnet.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# NOORCHAIN Phase 2 Testnet launcher placeholder.
+# The real testnet logic (chain init, keys, genesis, nodes)
+# will be added later in the Testnet tooling steps.
+
+echo "NOORCHAIN Testnet launcher placeholder (Phase 2)"
