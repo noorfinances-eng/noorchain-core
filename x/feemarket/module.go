9e3ab88130db581fb63402ee30308aeb6fba355e
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:58:28 +0100

    Create module.go

---
+++ x/feemarket/module.go
@@ -0,0 +1,8 @@
+package feemarket
+
+// ModuleName defines the fee market module name for NOORCHAIN.
+const ModuleName = "feemarket"
+
+// AppModule is a placeholder for the fee market module.
+// Full Cosmos SDK + Ethermint fee handling will be added later.
+type AppModule struct{}
