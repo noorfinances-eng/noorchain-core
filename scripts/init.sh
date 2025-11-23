fd3830e61b0b21ec773a7327c33ae9e11b85901f
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:29:28 +0100

    Create init.sh

---
+++ scripts/init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# NOORCHAIN Phase 2 initialization script placeholder.
+# This will be expanded in later steps when the Testnet tooling is added.
+
+echo "NOORCHAIN init script placeholder (Phase 2)"
