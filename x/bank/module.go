8550e35a5ad66d691296fcbba8cc3b88e91b8510
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:55:26 +0100

    Create module.go

---
+++ x/bank/module.go
@@ -0,0 +1,8 @@
+package bank
+
+// ModuleName defines the bank module name for NOORCHAIN.
+const ModuleName = "bank"
+
+// AppModule is a placeholder for the bank module.
+// Full Cosmos SDK wiring will be added later in Phase 2.
+type AppModule struct{}
