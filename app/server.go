383d3f5d8b5130a53a57ffeed3463852fa1ae2a0
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:49:25 +0100

    Create server.go

---
+++ app/server.go
@@ -0,0 +1,7 @@
+package app
+
+// StartServer is a placeholder for the NOORCHAIN ABCI server launcher.
+// In Phase 2 it does nothing. Real Tendermint/CometBFT wiring will come later.
+func StartServer() error {
+	return nil
+}
