d14035b1220c7d98f49d412f08a0ec4b6cd39435
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:21:28 +0100

    Delete x directory

--- x/noorsignal/keeper/keeper.go
+++
@@ -1,5 +0,0 @@
-package keeper
-
-// Keeper V1 minimal pour le module noorsignal.
-// On ajoutera la structure réelle et les méthodes plus tard.
-type Keeper struct{}
--- x/noorsignal/module.go
+++
@@ -1,4 +0,0 @@
-package noorsignal
-
-// Pour l'instant, ce fichier est un squelette minimal.
-// On ajoutera plus tard AppModule, AppModuleBasic, etc. pour Cosmos SDK 0.50.
