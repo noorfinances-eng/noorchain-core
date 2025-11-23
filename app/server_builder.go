2741c7958224c307c1b25a1ca27a1e4823f255b3
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:03:58 +0100

    Create server_builder.go

---
+++ app/server_builder.go
@@ -0,0 +1,7 @@
+package app
+
+// BuildServer prepares the NOORCHAIN server instance.
+// In Phase 2 this remains a placeholder without CometBFT wiring.
+func BuildServer(app *App) error {
+	return nil
+}
