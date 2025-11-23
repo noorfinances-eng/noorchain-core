c780b0720c87d453fc2f7e534e04898790725d66
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:54:42 +0100

    Create module.go

---
+++ x/auth/module.go
@@ -0,0 +1,8 @@
+package auth
+
+// ModuleName defines the auth module name for NOORCHAIN.
+const ModuleName = "auth"
+
+// AppModule is a placeholder for the authentication module.
+// It will be wired with the Cosmos SDK auth module in a later step.
+type AppModule struct{}
