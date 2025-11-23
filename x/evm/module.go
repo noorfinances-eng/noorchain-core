819d1fbf1a24d7f175919fe0d6822fc79ebec2d3
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:57:37 +0100

    Create module.go

---
+++ x/evm/module.go
@@ -0,0 +1,8 @@
+package evm
+
+// ModuleName defines the EVM module name for NOORCHAIN.
+const ModuleName = "evm"
+
+// AppModule is a placeholder for the Ethermint EVM module.
+// Full wiring with Ethermint will be added later in Phase 2.
+type AppModule struct{}
