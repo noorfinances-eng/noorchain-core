ffc06f202c994b0991e58957962a3338293c82cb
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 17:35:08 +0100

    Update msg.go

--- x/noorsignal/types/msg.go
+++ x/noorsignal/types/msg.go
@@ -5,6 +5,8 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	errorsmod "cosmossdk.io/errors"
 )
 
 //
@@ -23,13 +25,13 @@ func (m MsgSubmitSignal) Type() string  { return "submit_signal" }
 
 func (m MsgSubmitSignal) ValidateBasic() error {
 	if m.Participant == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "participant address cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "participant address cannot be empty")
 	}
 	if m.Weight == 0 {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "weight must be >= 1")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "weight must be >= 1")
 	}
 	if m.Weight > 100 {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "weight must be <= 100")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "weight must be <= 100")
 	}
 	return nil
 }
@@ -61,10 +63,10 @@ func (m MsgValidateSignal) Type() string  { return "validate_signal" }
 
 func (m MsgValidateSignal) ValidateBasic() error {
 	if m.Curator == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "curator address cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "curator address cannot be empty")
 	}
 	if m.SignalId == 0 {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "signal_id must be >= 1")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "signal_id must be >= 1")
 	}
 	return nil
 }
@@ -104,13 +106,13 @@ func (m MsgAddCurator) Type() string  { return "add_curator" }
 
 func (m MsgAddCurator) ValidateBasic() error {
 	if m.Authority == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
 	}
 	if m.Curator == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "curator cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "curator cannot be empty")
 	}
 	if m.Level == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "level cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "level cannot be empty")
 	}
 	return nil
 }
@@ -136,10 +138,10 @@ func (m MsgRemoveCurator) Type() string  { return "remove_curator" }
 
 func (m MsgRemoveCurator) ValidateBasic() error {
 	if m.Authority == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
 	}
 	if m.Curator == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "curator cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "curator cannot be empty")
 	}
 	return nil
 }
@@ -156,8 +158,8 @@ func (m MsgRemoveCurator) GetSigners() []sdk.AccAddress {
 // MsgSetConfig
 // -----------------------------
 type MsgSetConfig struct {
-	Authority       string `json:"authority" yaml:"authority"`
-	BaseReward      string `json:"base_reward" yaml:"base_reward"`
+	Authority        string `json:"authority" yaml:"authority"`
+	BaseReward       string `json:"base_reward" yaml:"base_reward"`
 	MaxSignalsPerDay uint32 `json:"max_signals_per_day" yaml:"max_signals_per_day"`
 	EraIndex         uint64 `json:"era_index" yaml:"era_index"`
 	ParticipantRatio uint32 `json:"participant_ratio" yaml:"participant_ratio"`
@@ -169,15 +171,15 @@ func (m MsgSetConfig) Type() string  { return "set_config" }
 
 func (m MsgSetConfig) ValidateBasic() error {
 	if m.Authority == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidAddress, "authority cannot be empty")
 	}
 
 	if m.BaseReward == "" {
-		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "base_reward cannot be empty")
+		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "base_reward cannot be empty")
 	}
 
 	if (m.ParticipantRatio + m.CuratorRatio) != 100 {
-		return sdkerrors.Wrap(
+		return errorsmod.Wrap(
 			sdkerrors.ErrInvalidRequest,
 			fmt.Sprintf("participant_ratio + curator_ratio must equal 100 (got %d)",
 				m.ParticipantRatio+m.CuratorRatio),
