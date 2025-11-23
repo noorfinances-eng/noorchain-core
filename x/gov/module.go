e49b80dde1b0142cb21a5841ab7df61a27539cd0
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:56:54 +0100

    Create module.go

---
+++ x/gov/module.go
@@ -0,0 +1,8 @@
+package gov
+
+// ModuleName defines the governance module name for NOORCHAIN.
+const ModuleName = "gov"
+
+// AppModule is a placeholder for the governance module.
+// Full Cosmos SDK gov wiring will be added later in Phase 2.
+type AppModule struct{}
