7b9532c375de78cedb1e33ec2468b96af9092ddf
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:50:00 +0100

    Create state.go

---
+++ app/state.go
@@ -0,0 +1,10 @@
+package app
+
+// State represents the placeholder application state structure.
+// It will be expanded once the Cosmos SDK keepers and stores are added.
+type State struct{}
+
+// NewState returns an empty State placeholder.
+func NewState() State {
+	return State{}
+}
