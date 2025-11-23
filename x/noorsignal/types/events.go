f885171f66fb9716f8e842bb31ef4a81f5544c34
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 14:42:02 +0100

    Update events.go

--- x/noorsignal/types/events.go
+++ x/noorsignal/types/events.go
@@ -11,25 +11,47 @@ import (
 // -----------------------------------------------------------------------------
 
 const (
-	// Event émis lorsqu'un participant soumet un nouveau signal PoSS.
+	// Signal soumis par un participant
 	EventTypeSignalSubmitted = "poss.signal_submitted"
 
-	// Event émis lorsqu'un curator valide un signal.
+	// Signal validé par un curator
 	EventTypeSignalValidated = "poss.signal_validated"
 
-	// Clés d'attributs communes.
-	AttributeKeySignalID          = "signal_id"
-	AttributeKeyParticipant       = "participant"
-	AttributeKeyCurator           = "curator"
-	AttributeKeyWeight            = "weight"
-	AttributeKeyTotalReward       = "total_reward"
-	AttributeKeyRewardParticipant = "reward_participant"
-	AttributeKeyRewardCurator     = "reward_curator"
-	AttributeKeyBlockHeight       = "block_height"
+	// Curator ajouté / mis à jour
+	EventTypeCuratorAdded = "poss.curator_added"
+
+	// Curator désactivé
+	EventTypeCuratorRemoved = "poss.curator_removed"
+
+	// Configuration PoSS mise à jour
+	EventTypeConfigUpdated = "poss.config_updated"
 )
 
 // -----------------------------------------------------------------------------
-// Helpers pour construire les événements
+// Clés d'attributs pour tous les events PoSS
+// -----------------------------------------------------------------------------
+
+const (
+	AttrKeySignalID          = "signal_id"
+	AttrKeyParticipant       = "participant"
+	AttrKeyCurator           = "curator"
+	AttrKeyWeight            = "weight"
+	AttrKeyTotalReward       = "total_reward"
+	AttrKeyRewardParticipant = "reward_participant"
+	AttrKeyRewardCurator     = "reward_curator"
+	AttrKeyBlockHeight       = "block_height"
+
+	AttrKeyLevel             = "level"
+	AttrKeyAuthority         = "authority"
+	AttrKeyBaseReward        = "base_reward"
+	AttrKeyMaxSignalsPerDay  = "max_signals_per_day"
+	AttrKeyEraIndex          = "era_index"
+	AttrKeyParticipantRatio  = "participant_ratio"
+	AttrKeyCuratorRatio      = "curator_ratio"
+)
+
+// -----------------------------------------------------------------------------
+// Helpers pour construire les événements de signaux
 // -----------------------------------------------------------------------------
 
 // NewSignalSubmittedEvent construit un événement poss.signal_submitted
@@ -37,10 +59,10 @@ const (
 func NewSignalSubmittedEvent(sig Signal, blockHeight int64) sdk.Event {
 	return sdk.NewEvent(
 		EventTypeSignalSubmitted,
-		sdk.NewAttribute(AttributeKeySignalID, fmt.Sprintf("%d", sig.Id)),
-		sdk.NewAttribute(AttributeKeyParticipant, sig.Participant.String()),
-		sdk.NewAttribute(AttributeKeyWeight, fmt.Sprintf("%d", sig.Weight)),
-		sdk.NewAttribute(AttributeKeyBlockHeight, fmt.Sprintf("%d", blockHeight)),
+		sdk.NewAttribute(AttrKeySignalID, fmt.Sprintf("%d", sig.Id)),
+		sdk.NewAttribute(AttrKeyParticipant, sig.Participant.String()),
+		sdk.NewAttribute(AttrKeyWeight, fmt.Sprintf("%d", sig.Weight)),
+		sdk.NewAttribute(AttrKeyBlockHeight, fmt.Sprintf("%d", blockHeight)),
 	)
 }
 
@@ -49,13 +71,13 @@ func NewSignalSubmittedEvent(sig Signal, blockHeight int64) sdk.Event {
 func NewSignalValidatedEvent(sig Signal, blockHeight int64) sdk.Event {
 	return sdk.NewEvent(
 		EventTypeSignalValidated,
-		sdk.NewAttribute(AttributeKeySignalID, fmt.Sprintf("%d", sig.Id)),
-		sdk.NewAttribute(AttributeKeyParticipant, sig.Participant.String()),
-		sdk.NewAttribute(AttributeKeyCurator, sig.Curator.String()),
-		sdk.NewAttribute(AttributeKeyWeight, fmt.Sprintf("%d", sig.Weight)),
-		sdk.NewAttribute(AttributeKeyTotalReward, fmt.Sprintf("%d", sig.TotalReward)),
-		sdk.NewAttribute(AttributeKeyRewardParticipant, fmt.Sprintf("%d", sig.RewardParticipant)),
-		sdk.NewAttribute(AttributeKeyRewardCurator, fmt.Sprintf("%d", sig.RewardCurator)),
-		sdk.NewAttribute(AttributeKeyBlockHeight, fmt.Sprintf("%d", blockHeight)),
+		sdk.NewAttribute(AttrKeySignalID, fmt.Sprintf("%d", sig.Id)),
+		sdk.NewAttribute(AttrKeyParticipant, sig.Participant.String()),
+		sdk.NewAttribute(AttrKeyCurator, sig.Curator.String()),
+		sdk.NewAttribute(AttrKeyWeight, fmt.Sprintf("%d", sig.Weight)),
+		sdk.NewAttribute(AttrKeyTotalReward, fmt.Sprintf("%d", sig.TotalReward)),
+		sdk.NewAttribute(AttrKeyRewardParticipant, fmt.Sprintf("%d", sig.RewardParticipant)),
+		sdk.NewAttribute(AttrKeyRewardCurator, fmt.Sprintf("%d", sig.RewardCurator)),
+		sdk.NewAttribute(AttrKeyBlockHeight, fmt.Sprintf("%d", blockHeight)),
 	)
 }
