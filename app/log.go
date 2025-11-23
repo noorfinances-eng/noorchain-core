443ba714348da697f2527b173ac05df6e1885157
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 20:50:54 +0100

    Update log.go

--- app/log.go
+++ app/log.go
@@ -1,15 +1,14 @@
 package app
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"cosmossdk.io/log"
 )
 
-// NewNoorchainLogger returns a basic NOORCHAIN logger.
-//
-// For now, it returns a no-op logger (it does not print anything).
-// Later, this can be changed to log to stdout, files, or structured logs
-// depending on how NOORCHAIN nodes are deployed.
-func NewNoorchainLogger() sdk.Logger {
-	return log.NewNopLogger()
+// GetLogger retourne le logger principal de l'application.
+// V1 : simple wrapper autour du logger de BaseApp.
+func (a *App) GetLogger() log.Logger {
+	if a == nil || a.BaseApp == nil {
+		return log.NewNopLogger()
+	}
+	return a.BaseApp.Logger()
 }
