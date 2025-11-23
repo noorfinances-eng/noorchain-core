3bdc027901d031ef03b87f6d2418a510ce9c824a
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 22:27:04 +0100

    Create constants.go

---
+++ app/constants.go
@@ -0,0 +1,11 @@
+package app
+
+// High-level NOORCHAIN application constants for Phase 2.
+const (
+	AppName    = "noorchaind"
+	AppVersion = "0.1.0-phase2"
+
+	DefaultDenom        = "unur"
+	DefaultDisplayDenom = "NUR"
+	DefaultDecimals     = 18
+)
