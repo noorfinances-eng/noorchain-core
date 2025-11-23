b3f17423d9ecffa17d700f117a6215f2c0ad7b64
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 20:51:17 +0100

    Update modules_layout.go

--- app/modules_layout.go
+++ app/modules_layout.go
@@ -1,50 +1,17 @@
 package app
 
-// Ce fichier définit le "plan" des modules NOORCHAIN :
-// - ordre d'exécution des BeginBlockers / EndBlockers
-// - ordre d'initialisation du genesis
+// AppModules regroupe les modules de l'application NOORCHAIN.
 //
-// À ce stade, ce ne sont que des listes de noms (string) basées sur
-// les constantes définies dans modules.go. Plus tard, ces listes
-// serviront à configurer le ModuleManager réel.
-
-var (
-	// Modules exécutés dans BeginBlocker (au début de chaque bloc).
-	BeginBlockerOrder = []string{
-		ModuleUpgrade,
-		ModuleCrisis,
-		ModuleSlashing,
-		ModuleStaking,
-		ModuleGov,
-		ModuleEvm,
-		ModuleFeeMarket,
-		ModuleNoorSignal,
-	}
-
-	// Modules exécutés dans EndBlocker (à la fin de chaque bloc).
-	EndBlockerOrder = []string{
-		ModuleGov,
-		ModuleStaking,
-		ModuleEvm,
-		ModuleFeeMarket,
-		ModuleNoorSignal,
-	}
-
-	// Ordre d'initialisation du genesis (InitGenesis).
-	InitGenesisOrder = []string{
-		ModuleAuth,
-		ModuleBank,
-		ModuleStaking,
-		ModuleMint,
-		ModuleSlashing,
-		ModuleGov,
-		ModuleParams,
-		ModuleCrisis,
-		ModuleUpgrade,
-		ModuleIBC,
-		ModuleTransfer,
-		ModuleEvm,
-		ModuleFeeMarket,
-		ModuleNoorSignal,
-	}
-)
+// V1 minimal : on ne définit ici qu'une structure vide.
+// On la remplira plus tard quand on câblera réellement les modules
+// (auth, bank, staking, gov, evm, feemarket, noorsignal, etc.).
+type AppModules struct {
+	// Exemple pour plus tard :
+	// Auth      interface{}
+	// Bank      interface{}
+	// Staking   interface{}
+	// Gov       interface{}
+	// Evm       interface{}
+	// FeeMarket interface{}
+	// NoorSignal interface{}
+}
