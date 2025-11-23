48552facc9eecc5b4d2b3242dfaf0f4f3a3873a8
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 21:56:17 +0100

    Create module.go

---
+++ x/staking/module.go
@@ -0,0 +1,8 @@
+package staking
+
+// ModuleName defines the staking module name for NOORCHAIN.
+const ModuleName = "staking"
+
+// AppModule is a placeholder for the staking module.
+// The full staking keeper and configuration will be added later.
+type AppModule struct{}
