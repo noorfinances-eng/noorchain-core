bc21aa668186edb84d1b946a1e1e3e654bc03b02
noorfinances-eng <noorfinances@gmail.com>
2025-11-23 20:50:27 +0100

    Update app_cosmos_skeleton.go

--- app/app_cosmos_skeleton.go
+++ app/app_cosmos_skeleton.go
@@ -1,84 +1,33 @@
 package app
 
 import (
-	"io"
-
-	dbm "github.com/cosmos/cosmos-db"
+	"cosmossdk.io/log"
 	"github.com/cosmos/cosmos-sdk/baseapp"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
 )
 
-// NewNoorchainAppWithCosmos définit le constructeur "Cosmos" futur
-// pour l'application NOORCHAIN.
-//
-// Il utilise AppBuilder pour :
-// - construire un BaseApp réel (stores montés)
-// - construire la structure des keepers
-// - récupérer la configuration d'encodage
-// - construire le ModuleManager (AppModules) et appliquer l'ordre des modules.
-// - enregistrer les hooks (BeginBlock, EndBlock, InitGenesis).
-// - enregistrer les services gRPC (Msg + Query) des modules.
-func NewNoorchainAppWithCosmos(
-	logger sdk.Logger,
-	db dbm.DB,
-	traceStore io.Writer,
-	loadLatest bool,
-	appOpts interface{},
+// NewBaseNoorchainApp crée une instance minimale de l'application NOORCHAIN.
+// Pour l'instant, c'est un constructeur basique qui sera enrichi plus tard
+// avec les keepers, modules, etc.
+func NewBaseNoorchainApp(
+	logger log.Logger,
+	base *baseapp.BaseApp,
 ) *App {
-
-	// 1) Créer un builder avec les paramètres Cosmos standard.
-	builder := NewAppBuilder(
-		logger,
-		db,
-		traceStore,
-		loadLatest,
-		appOpts,
-	)
-
-	// 2) Construire la BaseApp via le builder.
-	var base *baseapp.BaseApp = builder.BuildBaseApp()
-
-	// 3) Construire la structure des keepers.
-	keepers := builder.BuildKeepers()
-
-	// 4) Récupérer la config d'encodage utilisée par ce builder.
-	encCfg := builder.EncodingConfig()
-
-	// 5) Créer le ModuleManager (AppModules) à partir des keepers + encCfg.
-	modules := NewAppModuleManager(keepers, encCfg)
-
-	// 6) Appliquer l'ordre des modules (BeginBlock, EndBlock, InitGenesis)
-	// défini dans modules_layout.go.
-	ConfigureModuleManagerOrder(modules.Manager)
-
-	// 7) Construire l'instance de l'application NOORCHAIN.
-	app := &App{
-		BaseApp:  base,
-		Name:     "NOORCHAIN",
-		Version:  "0.0.1-dev",
-		Keepers:  keepers,
-		Encoding: encCfg,
-		Modules:  modules,
+	if base == nil {
+		base = baseapp.NewBaseApp("noorchain", logger, nil, nil)
 	}
 
-	// 8) Enregistrer les services gRPC (Msg + Query) des modules
-	// via le ModuleManager, en utilisant la config d'encodage réelle.
-	if app.BaseApp != nil && app.Modules.Manager != nil {
-		configurator := module.NewConfigurator(
-			encCfg.Marshaler,
-			app.MsgServiceRouter(),
-			app.GRPCQueryRouter(),
-		)
-
-		app.Modules.Manager.RegisterServices(configurator)
-
-		// 9) Enregistrer les hooks de cycle de vie (BeginBlock / EndBlock / InitGenesis)
-		// auprès de BaseApp, pour que le ModuleManager soit appelé à chaque bloc.
-		app.SetBeginBlocker(app.BeginBlocker)
-		app.SetEndBlocker(app.EndBlocker)
-		app.SetInitChainer(app.InitChainer)
+	return &App{
+		BaseApp: base,
+		Name:    "noorchain",
+		Version: "0.1.0",
+		// Keepers, Encoding et Modules seront câblés plus tard.
 	}
+}
 
-	return app
+// Logger renvoie le logger de l'application en utilisant le logger de BaseApp.
+func (a *App) Logger() log.Logger {
+	if a == nil {
+		return log.NewNopLogger()
+	}
+	return a.BaseApp.Logger()
 }
