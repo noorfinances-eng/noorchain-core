ed67f05f2fb070dda3d3643cea51a4dd2f9c7adb
noorfinances-eng <noorfinances@gmail.com>
2025-11-24 09:05:35 +0100

    Create cosmos_keeper.go

---
+++ app/cosmos_keeper.go
@@ -0,0 +1,10 @@
+package app
+
+// CosmosKeepers is a placeholder struct grouping all Cosmos SDK keepers.
+// In Phase 2 this remains empty; real keepers will be added in later steps.
+type CosmosKeepers struct{}
+
+// NewCosmosKeepers returns an empty placeholder keeper set.
+func NewCosmosKeepers() CosmosKeepers {
+	return CosmosKeepers{}
+}
