50358b35d707cba88195db1f658d2b488974eae4
noorfinances-eng <noorfinances@gmail.com>
2025-11-24 09:06:07 +0100

    Create cosmos_keeper_builder.go

---
+++ app/cosmos_keeper_builder.go
@@ -0,0 +1,7 @@
+package app
+
+// BuildCosmosKeepers is a placeholder that will construct all Cosmos SDK keepers.
+// In Phase 2, no keepers are initialized yet.
+func BuildCosmosKeepers() CosmosKeepers {
+	return NewCosmosKeepers()
+}
