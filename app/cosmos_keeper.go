e2a597a6777e7c665d8f817a599da0f7205dc50a
noorfinances-eng <noorfinances@gmail.com>
2025-11-25 17:24:12 +0100

    Update cosmos_keeper.go

--- app/cosmos_keeper.go
+++ app/cosmos_keeper.go
@@ -2,7 +2,14 @@ package app
 
 // CosmosKeepers is a placeholder struct grouping all Cosmos SDK keepers.
 // In Phase 2 this remains empty; real keepers will be added in later steps.
-type CosmosKeepers struct{}
+type CosmosKeepers struct {
+	// AuthKeeper      interface{}
+	// BankKeeper      interface{}
+	// StakingKeeper   interface{}
+	// GovKeeper       interface{}
+	// EvmKeeper       interface{}
+	// FeeMarketKeeper interface{}
+}
 
 // NewCosmosKeepers returns an empty placeholder keeper set.
 func NewCosmosKeepers() CosmosKeepers {
