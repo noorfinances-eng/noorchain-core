e7326d96ec4bf59d959d1df329831754b3e463a7
noorfinances-eng <noorfinances@gmail.com>
2025-11-25 17:14:11 +0100

    Create cosmos_module_basics.go

---
+++ app/cosmos_module_basics.go
@@ -0,0 +1,9 @@
+package app
+
+import (
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+// ModuleBasics defines the basic module elements for NOORCHAIN.
+// Phase 2: empty, but required by Cosmos SDK and Ethermint.
+var ModuleBasics = module.BasicManager{}
