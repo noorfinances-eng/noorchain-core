e647f0ba11cb58a98765ac9a6c0b549b46332f3a
noorfinances-eng <noorfinances@gmail.com>
2025-11-26 18:37:12 +0100

    Update cosmos_module_basics.go

--- app/cosmos_module_basics.go
+++ app/cosmos_module_basics.go
@@ -3,10 +3,12 @@ package app
 import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authmodule "github.com/cosmos/cosmos-sdk/x/auth/module"
+	bankmodule "github.com/cosmos/cosmos-sdk/x/bank/module"
 )
 
 // ModuleBasics defines the basic module elements for NOORCHAIN.
-// Phase 2: now includes the AUTH module (first real Cosmos module).
+// Phase 2: now includes AUTH + BANK modules.
 var ModuleBasics = module.BasicManager(
 	authmodule.AppModuleBasic{},
+	bankmodule.AppModuleBasic{},
 )
