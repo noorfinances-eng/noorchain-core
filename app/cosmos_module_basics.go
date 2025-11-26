b13f39d415a23d2ea3b1aec33c321a7604e5786a
noorfinances-eng <noorfinances@gmail.com>
2025-11-26 18:53:15 +0100

    Update cosmos_module_basics.go

--- app/cosmos_module_basics.go
+++ app/cosmos_module_basics.go
@@ -6,13 +6,17 @@ import (
 	bankmodule "github.com/cosmos/cosmos-sdk/x/bank/module"
 	stakingmodule "github.com/cosmos/cosmos-sdk/x/staking/module"
 	govmodule "github.com/cosmos/cosmos-sdk/x/gov/module"
+	evmmodule "github.com/evmos/ethermint/x/evm/module"
+	feemarketmodule "github.com/evmos/ethermint/x/feemarket/module"
 )
 
 // ModuleBasics defines the basic module elements for NOORCHAIN.
-// Phase 2: includes AUTH + BANK + STAKING + GOV modules.
+// Phase 2: includes AUTH + BANK + STAKING + GOV + EVM + FEEMARKET modules.
 var ModuleBasics = module.BasicManager(
 	authmodule.AppModuleBasic{},
 	bankmodule.AppModuleBasic{},
 	stakingmodule.AppModuleBasic{},
 	govmodule.AppModuleBasic{},
+	evmmodule.AppModuleBasic{},
+	feemarketmodule.AppModuleBasic{},
 )
