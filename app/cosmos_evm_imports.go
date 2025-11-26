dae57861c5b48d9912f859b844036b6cc4696af2
noorfinances-eng <noorfinances@gmail.com>
2025-11-26 21:39:30 +0100

    Update cosmos_evm_imports.go

--- app/cosmos_evm_imports.go
+++ app/cosmos_evm_imports.go
@@ -1,21 +1,18 @@
 package app
 
-// Phase 2: Import placeholders for Ethermint EVM and FeeMarket modules.
-// We do NOT wire anything yet. This only prepares structure.
-
 import (
-	evmmodule "github.com/evmos/ethermint/x/evm/module"
+	evm "github.com/evmos/ethermint/x/evm"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
-
-	feemarketmodule "github.com/evmos/ethermint/x/feemarket/module"
+	feemarket "github.com/evmos/ethermint/x/feemarket"
 	feemarkettypes "github.com/evmos/ethermint/x/feemarket/types"
 )
 
-// Placeholder references to avoid unused import errors in Phase 2.
+// CosmosEVMImports keeps references to Ethermint modules to ensure
+// they are pulled by Go modules in Phase 2.
 var (
-	_ = evmmodule.AppModuleBasic{}
-	_ = evmtypes.MsgEthereumTx{}
+	_ = evm.AppModuleBasic{}
+	_ = evmtypes.ModuleName
 
-	_ = feemarketmodule.AppModuleBasic{}
-	_ = feemarkettypes.MsgUpdateParams{}
+	_ = feemarket.AppModuleBasic{}
+	_ = feemarkettypes.ModuleName
 )
