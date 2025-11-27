760a1bb1e5fe04af3a09cd1fddb092462fb82d2d
noorfinances-eng <noorfinances@gmail.com>
2025-11-27 07:56:34 +0000

    Add encoding config (encoding.go)

--- app/encoding.go
+++ app/encoding.go
@@ -1,11 +1,36 @@
 package app
 
-// EncodingConfig represents the placeholder for all
-// codecs and interface registries used by the application.
-// It will be fully implemented when Cosmos SDK modules are wired.
-type EncodingConfig struct{}
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+)
 
-// MakeEncodingConfig returns an empty placeholder config for now.
-func MakeEncodingConfig() EncodingConfig {
-	return EncodingConfig{}
+// EncodingConfig defines the application encoding configuration.
+// It is used to initialize all codec-related features.
+type EncodingConfig struct {
+	InterfaceRegistry codectypes.InterfaceRegistry
+	Marshaler         codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
+
+// MakeEncodingConfig creates an EncodingConfig for testing or app setup.
+func MakeEncodingConfig(modules module.BasicManager) EncodingConfig {
+	amino := codec.NewLegacyAmino()
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	marshaler := codec.NewProtoCodec(interfaceRegistry)
+	txConfig := tx.NewTxConfig(marshaler, tx.DefaultSignModes)
+
+	// Register modules interfaces & codecs
+	modules.RegisterLegacyAminoCodec(amino)
+	modules.RegisterInterfaces(interfaceRegistry)
+
+	return EncodingConfig{
+		InterfaceRegistry: interfaceRegistry,
+		Marshaler:         marshaler,
+		TxConfig:          txConfig,
+		Amino:             amino,
+	}
 }
