674f139e84044ecb215ca9e25e311439cc1cd6f7
noorfinances-eng <noorfinances@gmail.com>
2025-11-27 08:27:02 +0000

    Add nord main binary placeholder (Phase 2)

--- cmd/noord/main.go
+++ cmd/noord/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/tendermint/tendermint/libs/log"
+
+	dbm "github.com/tendermint/tm-db"
+
+	noorapp "github.com/noorfinances-eng/noorchain-core/app"
+)
 
 func main() {
-	fmt.Println("NOORCHAIN node placeholder (Phase 2 not wired yet)")
+	// Logger: simple stdout logger
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+
+	// Database: in-memory for Phase 2
+	db := dbm.NewMemDB()
+
+	// Create NOORCHAIN App
+	app := noorapp.New(logger, db)
+
+	// Placeholder for Phase 2
+	fmt.Println("NOORCHAIN node placeholder (Phase 2 - basic App created)")
+	fmt.Println("App loaded:", app.BaseApp.Name())
+
+	// NOTE:
+	// In Phase 3+ we will add:
+	// - CLI commands
+	// - Tendermint server startup
+	// - Genesis loading
+	// - PoSS module wiring
 }
