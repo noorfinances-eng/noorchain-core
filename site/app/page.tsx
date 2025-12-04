4d93c8d6e9bdd92a5f05644fdddf606a6e7d2880
noorfinances-eng <noorfinances@gmail.com>
2025-12-04 21:57:53 +0100

    Create page.tsx

---
+++ site/app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className="p-10">
+      <h1 className="text-4xl font-bold mb-4">NOORCHAIN</h1>
+      <p className="text-lg text-gray-700">
+        Social Signal Blockchain powered by PoSS.
+      </p>
+    </main>
+  );
+}
