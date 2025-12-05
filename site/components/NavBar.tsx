83dfabc2dc3e7b3c4d98f9fa8aa3724be559b5e3
noorfinances-eng <noorfinances@gmail.com>
2025-12-05 11:22:46 +0100

    Update NavBar.tsx

--- site/components/NavBar.tsx
+++ site/components/NavBar.tsx
@@ -4,18 +4,38 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav className="w-full flex items-center justify-between px-6 py-4 border-b">
-      <div className="text-xl font-bold">NOORCHAIN</div>
+    <nav className="w-full border-b bg-white">
+      <div className="container flex items-center justify-between py-4">
+        <div className="text-xl font-bold tracking-tight">
+          NOORCHAIN
+        </div>
 
-      <div className="flex gap-6 text-sm">
-        <Link href="/">Home</Link>
-        <Link href="/technology">Technology</Link>
-        <Link href="/poss">PoSS</Link>
-        <Link href="/curators">Curators</Link>
-        <Link href="/genesis">Genesis</Link>
-        <Link href="/roadmap">Roadmap</Link>
-        <Link href="/docs">Docs</Link>
-        <Link href="/legal">Legal</Link>
+        <div className="flex gap-5 text-sm text-gray-700">
+          <Link href="/" className="hover:text-black">
+            Home
+          </Link>
+          <Link href="/technology" className="hover:text-black">
+            Technology
+          </Link>
+          <Link href="/poss" className="hover:text-black">
+            PoSS
+          </Link>
+          <Link href="/curators" className="hover:text-black">
+            Curators
+          </Link>
+          <Link href="/genesis" className="hover:text-black">
+            Genesis
+          </Link>
+          <Link href="/roadmap" className="hover:text-black">
+            Roadmap
+          </Link>
+          <Link href="/docs" className="hover:text-black">
+            Docs
+          </Link>
+          <Link href="/legal" className="hover:text-black">
+            Legal
+          </Link>
+        </div>
       </div>
     </nav>
   );
