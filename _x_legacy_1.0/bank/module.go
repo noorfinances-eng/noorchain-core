9488811a83e7f7750f2e4d6fced9c34295d1c0cb
noorfinances-eng <noorfinances@gmail.com>
2025-12-18 21:07:54 +0000

    chore: remove legacy 1.0 folders

--- _x_legacy_1.0/bank/module.go
+++
@@ -1,8 +0,0 @@
-package bank
-
-// ModuleName defines the bank module name for NOORCHAIN.
-const ModuleName = "bank"
-
-// AppModule is a placeholder for the bank module.
-// Full Cosmos SDK wiring will be added later in Phase 2.
-type AppModule struct{}
