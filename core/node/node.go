2ee15824046c2063917e219d289e176a7f2a038c
noorfinances-eng <noorfinances@gmail.com>
2025-12-25 17:21:00 +0000

    fix: logger interface usage

--- core/node/node.go
+++ core/node/node.go
@@ -1,29 +1,36 @@
 package node
 
 import (
-	"fmt"
 	"os"
 
 	"noorchain-evm-l1/core/config"
 )
 
+type Logger interface {
+	Println(v ...any)
+}
+
 type Node struct {
-	cfg config.Config
+	cfg    config.Config
+	logger Logger
 }
 
 func New(cfg config.Config) *Node {
-	return &Node{cfg: cfg}
+	return &Node{
+		cfg:    cfg,
+		logger: newLogger(),
+	}
 }
 
 func (n *Node) Start() {
 	if err := os.MkdirAll(n.cfg.DataDir, 0o755); err != nil {
 		panic(err)
 	}
-	fmt.Println("node started")
-	fmt.Println("chain-id:", n.cfg.ChainID)
-	fmt.Println("data-dir:", n.cfg.DataDir)
+	n.logger.Println("node started")
+	n.logger.Println("chain-id:", n.cfg.ChainID)
+	n.logger.Println("data-dir:", n.cfg.DataDir)
 }
 
 func (n *Node) Stop() {
-	fmt.Println("node stopped")
+	n.logger.Println("node stopped")
 }
