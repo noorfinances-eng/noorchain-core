7cc621b0dda19d689b68c722012d89fca82674da
noorfinances-eng <noorfinances@gmail.com>
2025-12-25 16:55:40 +0000

    Fix app wiring and deps for state queries

--- app/module.go
+++ app/module.go
@@ -5,6 +5,7 @@ import (
 	authmodule "github.com/cosmos/cosmos-sdk/x/auth"
 	bankmodule "github.com/cosmos/cosmos-sdk/x/bank"
 	genutilmodule "github.com/cosmos/cosmos-sdk/x/genutil"
+	paramsmodule "github.com/cosmos/cosmos-sdk/x/params"
 	stakingmodule "github.com/cosmos/cosmos-sdk/x/staking"
 )
 
@@ -14,4 +15,5 @@ var ModuleBasics = module.NewBasicManager(
 	bankmodule.AppModuleBasic{},
 	stakingmodule.AppModuleBasic{},
 	genutilmodule.AppModuleBasic{},
+	paramsmodule.AppModuleBasic{}, // ajout du module params
 )
