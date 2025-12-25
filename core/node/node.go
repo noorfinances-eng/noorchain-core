6e318051752904b0d1e28e663ec15c0818588849
noorfinances-eng <noorfinances@gmail.com>
2025-12-25 17:26:34 +0000

    m2.2: add explicit node state machine

--- core/node/node.go
+++ core/node/node.go
@@ -13,12 +13,21 @@ type Logger interface {
 	Println(v ...any)
 }
 
+type State string
+
+const (
+	StateInit     State = "INIT"
+	StateRunning  State = "RUNNING"
+	StateStopping State = "STOPPING"
+)
+
 type Node struct {
 	cfg     config.Config
 	logger  Logger
 	network *network.Network
 	ctx     context.Context
 	cancel  context.CancelFunc
+	state   State
 }
 
 func New(cfg config.Config) *Node {
@@ -29,6 +38,7 @@ func New(cfg config.Config) *Node {
 		network: network.New(cfg.P2PAddr),
 		ctx:     ctx,
 		cancel:  cancel,
+		state:   StateInit,
 	}
 }
 
@@ -36,12 +46,14 @@ func (n *Node) Start() {
 	if err := os.MkdirAll(n.cfg.DataDir, 0o755); err != nil {
 		panic(err)
 	}
+
+	n.state = StateRunning
 	n.logger.Println("node started")
+	n.logger.Println("state:", n.state)
 	n.logger.Println("chain-id:", n.cfg.ChainID)
 	n.logger.Println("data-dir:", n.cfg.DataDir)
 
 	n.network.Start()
-
 	go n.loop()
 }
 
@@ -55,12 +67,15 @@ func (n *Node) loop() {
 			n.logger.Println("node loop stopped")
 			return
 		case <-ticker.C:
-			n.logger.Println("tick")
+			n.logger.Println("tick | state:", n.state)
 		}
 	}
 }
 
 func (n *Node) Stop() {
+	n.state = StateStopping
+	n.logger.Println("state:", n.state)
+
 	n.cancel()
 	n.network.Stop()
 	n.logger.Println("node stopped")
