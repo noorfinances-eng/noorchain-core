57ad7b1f03fc8c7af2050e0fe112880776a52428
noorfinances-eng <noorfinances@gmail.com>
2025-12-25 17:22:42 +0000

    core: real TCP listener for network bootstrap

--- core/network/network.go
+++ core/network/network.go
@@ -1,9 +1,13 @@
 package network
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 type Network struct {
 	addr string
+	ln   net.Listener
 }
 
 func New(addr string) *Network {
@@ -11,9 +15,17 @@ func New(addr string) *Network {
 }
 
 func (n *Network) Start() {
+	ln, err := net.Listen("tcp", n.addr)
+	if err != nil {
+		panic(err)
+	}
+	n.ln = ln
 	fmt.Println("network listening on", n.addr)
 }
 
 func (n *Network) Stop() {
+	if n.ln != nil {
+		_ = n.ln.Close()
+	}
 	fmt.Println("network stopped")
 }
